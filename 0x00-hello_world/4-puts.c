aadeaead1dbc74bb44256e4d793d87298c3e4546
Hanchi Mouafek <mouafek66@outlook.fr>
2022-12-09 09:19:35 +0000

    task 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+// print a line of string in the standard output and return zero
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
