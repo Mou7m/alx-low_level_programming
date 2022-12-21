47c743db0c9875e54157a3a0f38dda8f401c001c
mouafek <mouafek66@outlook.fr>
2022-12-21 09:39:17 +0000

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- *main - print _putchar followed by a new line
- *
- *Return: Always 0 (success)
- */
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
 	return (0);
 }
